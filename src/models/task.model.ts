82767134c13f9704eb248940846da0b7279dbf95
fhanifel <felix_hanifel@web.de>
2017-05-12 15:46:45 +0200

    Added task.Model and task.Service

---
+++ src/models/task.model.ts
@@ -0,0 +1,13 @@
+export class Task {
+  id: number;
+  state: string;
+  title: string;
+  description: string;
+  responseType: string;
+  difficulty: number;
+  rating: number;
+  from: string;
+  to: string;
+  created_at: string;
+  updated_at: string;
+}
---
+++ src/services/task.service.ts
@@ -0,0 +1,46 @@
+import {Task} from "../models/task.model";
+import {AuthService} from "./auth.service";
+
+export class TaskService {
+  constructor(private authService: AuthService) {
+  }
+
+
+  createTask(task: Task) {
+    firebase.database().ref('tasks/').set(task);
+  }
+
+  getAllTasksFromMe() {
+    let uid = this.authService.getAuthenticatedUser().uid;
+
+    return firebase.database().ref('tasks/').once('value');
+  }
+
+  getTaskWithId(taskId: string){
+
+  }
+
+  getAllTasksForMe() {
+
+  }
+
+  deleteTask(taskId: number) {
+
+  }
+
+  editTask(taskId: number) {
+
+  }
+
+  rateTask(taskId: number, rate: string){
+
+
+  }
+
+  changeState(taskID: number, state: string){
+
+  }
+
+
+
+}
