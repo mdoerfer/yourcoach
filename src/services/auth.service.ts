64890eacda120358171d7aec898fc00dd14f79b4
Dirk Englert <dirk.englert@web.de>
2017-05-13 21:12:12 +0200

    Removed unused methods from services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -42,37 +42,4 @@ export class AuthService {
   resetPassword(email: string) {
     return firebase.auth().sendPasswordResetEmail(email);
   }
-
-  /**
-   * Get the currently authenticated user
-   *
-   * @returns {firebase.User|null}
-   */
-  getAuthenticatedUser() {
-    return firebase.auth().currentUser;
-  }
-
-  /**
-   * Sets the user node in the firebase database to the passed object
-   * and overrides any properties that might have existed prior to calling
-   * the method
-   *
-   * @param ref
-   */
-  setUserRef(ref: object) {
-    let uid = this.getAuthenticatedUser().uid;
-
-    firebase.database().ref('users/' + uid).set(ref);
-  }
-
-  /**
-   * Returns the promise containing the user node
-   *
-   * @returns {firebase.Promise<any>}
-   */
-  getUserRef() {
-    let uid = this.getAuthenticatedUser().uid;
-
-    return firebase.database().ref('/users/' + uid).once('value');
-  }
 }
