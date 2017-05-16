5329880e36d832f367dd97023eef9412d7a8346e
MaeElese <mae.pfeifer@gmail.com>
2017-05-16 13:47:13 +0200

    Added tabs to student task page

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
 
 /**
- * Generated class for the StudentTaskPage page.
+ * Generated class for the SettingsPage page.
  *
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
 @IonicPage()
 @Component({
-  selector: 'page-student-task',
-  templateUrl: 'student-task.html',
+  selector: 'page-settings',
+  templateUrl: 'settings.html',
 })
-export class StudentTaskPage {
-  taskchange: string = "open";
+export class SettingsPage {
+
 
 }
