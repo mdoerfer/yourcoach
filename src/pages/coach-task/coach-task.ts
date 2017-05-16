b3fa01d97c1a9299fc7f40911ee7b1438e9830bc
MaeElese <mae.pfeifer@gmail.com>
2017-05-16 10:04:26 +0200

    cleaned up coach task after testing

--- src/pages/coach-task/coach-task.ts
+++ src/pages/coach-task/coach-task.ts
@@ -1,24 +1,12 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage} from 'ionic-angular';
 
-/**
- * Generated class for the CoachTaskPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-coach-task',
   templateUrl: 'coach-task.html',
 })
 export class CoachTaskPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CoachTaskPage');
-  }
+  taskchange: string = "open";
 
 }
