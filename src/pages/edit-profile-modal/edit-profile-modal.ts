e9c357b339bff7ad5bb417271e1657f6cda7268c
fhanifel <felixhanifel@gmail.com>
2017-06-01 18:04:14 +0200

    Add From to EditModal

--- src/pages/edit-profile-modal/edit-profile-modal.ts
+++ src/pages/edit-profile-modal/edit-profile-modal.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {NavParams, ViewController} from "ionic-angular";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
-  selector: 'edit-profile-modal',
+  selector: 'page-edit-profile-modal',
   templateUrl: 'edit-profile-modal.html',
 })
 export class EditProfileModalPage implements OnInit{
+
+  profileForm: FormGroup;
 
 
   constructor(public viewCtrl: ViewController,
@@ -19,13 +22,26 @@ export class EditProfileModalPage implements OnInit{
    * Initialize the page
    */
   ngOnInit() {
+
     this.initializeForm();
   }
-
   /**
    * Initialize the form
    */
   private initializeForm() {
+    let formData = {
+      name: null,
+      description: null,
+    };
+
+
+    //Create form
+    this.profileForm = new FormGroup({
+      name: new FormControl(formData.name, Validators.required),
+      description: new FormControl(formData.description, Validators.required),
+    });
+
+    console.log(this.profileForm);
 
   }
 
