ff0cc94a92aaf2a328342a0d5a0df9b68337e702
bbeaarr <bortsimon@gmail.com>
2017-06-20 18:34:05 +0200

    Created statistic calculation

--- src/pages/statistics/statistics.ts
+++ src/pages/statistics/statistics.ts
@@ -15,14 +15,81 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class StatisticsPage {
 
   doneTasks: any;
+  activeSince: string;
+
+  countEasy: number;
+  countMiddle: number;
+  countHard: number;
+
+  percentEasy: number;
+  percentMiddle: number;
+  percentHard: number;
+
+  averageRating: number;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams) {
 
     this.doneTasks = this.navParams.get('doneTasks');
-    console.log(this.doneTasks);
+
+    this.calculateActiveSince();
+    this.countDiffuculty();
+    this.calculatePercentageDifficulty();
+    this.calculateAverageRating();
   }
 
+  calculateActiveSince(){
+    let min = new Date().valueOf();
+
+    for(let i = 0; i < this.doneTasks.length; i++){
+      if (min >= this.doneTasks[i].created_at){
+        min = this.doneTasks[i].created_at;
+
+      }
+    }
+    let d = new Date(min);
+    this.activeSince = d.getDate() + '.' + (d.getMonth()+1) + '.' + d.getFullYear();
+  }
+
+  countDiffuculty(){
+    let countEasy = 0;
+    let countMiddle = 0;
+    let countHard = 0;
+
+    for(let i = 0; i < this.doneTasks.length; i++){
+
+      switch(this.doneTasks[i].difficulty) {
+        case 'Einfach':
+          countEasy++;
+          break;
+        case 'Mittel':
+          countMiddle++;
+          break;
+        case 'Schwer':
+          countHard++;
+      }
+    }
+    this.countEasy = countEasy;
+    this.countMiddle = countMiddle;
+    this.countHard = countHard;
+  }
+
+  calculatePercentageDifficulty(){
+    this.percentEasy = Math.round(this.countEasy / this.doneTasks.length * 100);
+    this.percentMiddle = Math.round(this.countMiddle / this.doneTasks.length * 100);
+    this.percentHard = Math.round(this.countHard / this.doneTasks.length * 100);
+  }
+
+  calculateAverageRating(){
+    let rating = 0;
+
+    for(let i = 0; i < this.doneTasks.length; i++){
+      rating += this.doneTasks[i].rating;
+      }
+
+      this.averageRating = Math.round(rating / this.doneTasks.length *10) / 10;
+
+    }
 
 
 }
