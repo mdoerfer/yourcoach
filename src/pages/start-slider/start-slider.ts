6e7254de2483cd72ce119f8c8a3df4ada297cffa
MaeElese <mae.pfeifer@gmail.com>
2017-06-21 11:35:46 +0200

    Added demo code for start slider

--- src/pages/start-slider/start-slider.ts
+++ src/pages/start-slider/start-slider.ts
@@ -21,4 +21,22 @@ export class StartSliderPage {
     console.log('ionViewDidLoad StartSliderPage');
   }
 
+  slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "assets/img/slide1.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/img/slide2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/slide3.png",
+    }
+  ];
+
 }
