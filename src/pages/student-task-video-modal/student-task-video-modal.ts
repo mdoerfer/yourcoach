1a393274c9968c12db6dda6b1faf99c297b945ba
tapiwan <dirk.englert@web.de>
2017-06-21 19:10:18 +0200

    Prepared response video modal

--- src/pages/student-task-video-modal/student-task-video-modal.ts
+++ src/pages/student-task-video-modal/student-task-video-modal.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {NavParams, ViewController} from "ionic-angular";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'page-student-task-video-modal',
@@ -9,37 +8,20 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class StudentTaskVideoModalPage implements OnInit{
 
   task: any;
-  responseForm: FormGroup;
+  response: any = null;
 
   constructor(public viewCtrl: ViewController,
               private navParams: NavParams) {
 
     this.task = this.navParams.get('task');
-
   }
 
   /**
    * Initialize the page
    */
   ngOnInit() {
-    this.initializeForm();
+
   }
-
-  /**
-   * Initialize the form
-   */
-  private initializeForm() {
-
-    let formData = {
-      response: null,
-    };
-
-    //Create form
-    this.responseForm = new FormGroup({
-      response: new FormControl(formData.response, Validators.required),
-    });
-  }
-
 
   /**
    * Closes text modal
@@ -47,13 +29,6 @@ export class StudentTaskVideoModalPage implements OnInit{
   dismissModal(){
     this.viewCtrl.dismiss();
 
-  }
-
-  /**
-   * Closes text modal with data
-   */
-  submitForm(){
-    this.viewCtrl.dismiss(this.responseForm.get('response').value);
   }
 
 
