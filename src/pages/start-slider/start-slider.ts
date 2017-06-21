9618a72db94af213911408f9c0bb3194c872a5b6
MaeElese <mae.pfeifer@gmail.com>
2017-06-21 11:50:13 +0200

    Completed implementation of demo

--- src/pages/start-slider/start-slider.ts
+++ src/pages/start-slider/start-slider.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {RoleChoicePage} from "../role-choice/role-choice";
 
 /**
  * Generated class for the StartSliderPage page.
@@ -25,18 +26,22 @@ export class StartSliderPage {
     {
       title: "Welcome to the Docs!",
       description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
-      image: "assets/img/slide1.png",
+      image: "assets/images/slide1.png",
     },
     {
       title: "What is Ionic?",
       description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
-      image: "assets/img/slide2.png",
+      image: "assets/images/slide2.png",
     },
     {
       title: "What is Ionic Cloud?",
       description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
-      image: "assets/img/slide3.png",
+      image: "assets/images/slide3.png",
     }
   ];
+
+  goToRoleChoice() {
+    this.navCtrl.push(RoleChoicePage);
+  }
 
 }
