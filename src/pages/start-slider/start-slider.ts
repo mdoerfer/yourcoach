bd64c1ca4f1a85e6b995b189c375974fdece022e
mirjamdorfer <mir.doerfer.14@dhbw-mosbach.de>
2017-06-21 16:10:32 +0200

    text fix start-slider3

--- src/pages/start-slider/start-slider.ts
+++ src/pages/start-slider/start-slider.ts
@@ -38,7 +38,7 @@ export class StartSliderPage {
     },
     {
       title: "Näher an den Schülern",
-      description: "Du bist Coach und willst deine Schüiler rund um die Uhr betreuen? Lade <b>deine Schüler</b> ein und gib ihnen Aufgaben.",
+      description: "Du bist Coach und willst deine Schüler rund um die Uhr betreuen? Lade <b>deine Schüler</b> ein und gib ihnen Aufgaben.",
       image: "assets/images/slide3.png",
     }
   ];
